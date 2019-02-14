8b85cc87eeadfb01f3c6d66995a55a8b15cf1c50
tatianamiranda16 <47321327+tatianamiranda16@users.noreply.github.com>
2019-02-14 08:03:02 -0300

    Add files via upload

--- 8-TPs/jsFerreteConstruccion.js
+++ 8-TPs/jsFerreteConstruccion.js
@@ -5,14 +5,44 @@ C.	Para hacer un contrapiso de 1m x 1m se necesitan 2 bolsas de cemento y 3 de c
 */
 
 function Rectangulo () 
-{
+{var largodeterreno;
+    var anchodelterreno;
+    var alambre;
+    largodeterreno=document.getElementById("Largo").value;
+    largodeterreno=parseFloat(largodeterreno);
+anchodelterreno=document.getElementById("Ancho").value;
+anchodelterreno=parseFloat(anchodelterreno);
+alambre= (anchodelterreno * 2 + largodeterreno * 2) * 3;
+alert("el alambre a comprar es " + alambre);
+
 
 }
 function Circulo () 
-{
-	
+{var radio;
+    var alambre;
+    var comprar
+    radio=document.getElementById("Radio").value;
+	radio=parseFloat(radio)
+    radio=radio.toFixed (2);
+    alambre=(Math.PI * radio * 2) * 3;
+    alert("el alambre a comprar es "+ alambre +"cm");
+    
+    
 }
 function Materiales () 
-{
+{var cemento;
+    var cal;
+    var largo;
+    var ancho;
+    var superficie;
+var Materiales
+    largo=parseFloat(document.getElementById("Largo").value);
+    ancho=parseFloat(document.getElementById("Ancho").value);
+    superficie= largo * ancho
+    cal=superficie * 3 ; 
+    cemento= superficie * 2 
+    alert("el m2 es " + superficie + ("necesito ") + cal + ("de cal ") + ("y de cemento" + cemento));
+
+     
 	
 }
--- 8-TPs/jsFerreteFacturacion.js
+++ 8-TPs/jsFerreteFacturacion.js
@@ -4,14 +4,49 @@ B.	Ingresar tres precios de productos y mostrar el promedio de los mismos.
 C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
 function Sumar () 
-{
+{var productos
+    var productodos
+    var productotres
+    var PrecioFinal
+    productos=document.getElementById("PrecioUno").value;
+productos=parseFloat(productos);
+productodos=document.getElementById("PrecioDos").value;
+productodos=parseFloat(productodos);
+productotres=document.getElementById("PrecioTres").value;
+productotres=parseFloat(productotres);
+PrecioFinal=productos + productodos + productotres;
+alert("el precio final es " + PrecioFinal);
+
 	
 }
 function Promedio () 
-{
-	
+{var productos
+    var productodos;
+    var productotres;
+    var PrecioFinal;
+    productos=document.getElementById("PrecioUno").value;
+    productos=parseFloat(productos);
+    productodos=document.getElementById("PrecioDos").value;
+    productodos=parseFloat(productodos);
+    productotres=document.getElementById("PrecioTres").value;
+    productotres=parseFloat(productotres);
+    PrecioFinal= (productos + productodos + productotres) / 3;
+	alert("el precio final es " + PrecioFinal);
 }
 function PrecioFinal () 
-{
+{var productos
+    var productodos
+    var productotres
+    var PrecioFinal
+    var precioconiva
+    productos=document.getElementById("PrecioUno").value;
+    productos=parseFloat(productos);
+    productodos=document.getElementById("PrecioDos").value;
+    productodos=parseFloat(productodos);
+    productotres=document.getElementById("PrecioTres").value;
+    productotres=parseFloat(productotres);
+    PrecioFinal= (productos + productodos + productotres) * 21 / 100;
+    precioconiva=productos + productodos + productotres - PrecioFinal;
+    alert("el precio final es " + precioconiva);
 	
 }
